08286b1564b91b84c8b258337b339a6167cd32e3
Dileep T Paul <dileeptpaul2019@gmail.com>
2020-05-20 23:22:19 +0530

    Updated libraries for bootstrap,modal,i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {Ng5SliderModule} from 'ng5-slider';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-
+import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
+import {DatePipe} from '@angular/common';
+import {ModalModule} from 'ngx-bootstrap/modal';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
+    FormsModule, Ng5SliderModule, HttpClientModule, ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule {
